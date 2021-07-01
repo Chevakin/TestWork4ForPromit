d5845dfdf18dada1d3eeb61ec6031bfcc19a02fb
Andrey Chevakin <pandromeda@bk.ru>
2021-07-01 15:58:14 +0500

    Добавьте файлы проекта.

---
+++ TestWork4ForPromit.Uploader/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TestWork4ForPromit.Uploader
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
